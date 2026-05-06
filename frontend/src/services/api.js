0778a14722221e59a25471a218cacad24a2e5b76
Harsh kumar <hkumar51622@gmail.com>
2026-05-06 21:57:55 +0530

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8000/api/',
+  baseURL: 'https://anytime-trips-1.onrender.com/api/',
 });
 
 api.interceptors.request.use((config) => {
